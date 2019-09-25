1433e3a95e6deee64391f2785ad233aff4b6b032
Hyunwoo Lee <aeharvlee@gmail.com>
2019-09-26 02:31:36 +0900

    [Update]: deploy script and truffle-config
    * Update deploy scripts
    	* Hide accounts information in .secret.json file for security issues.
    * Update truffle config
    	* More felxible and secure than before.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,29 @@
 const Cloudbric = artifacts.require("./Cloudbric.sol");
+const fs = require('fs');
+const secret = fs.readFileSync('../.secret.json', 'utf-8');
+const parsedSecret = JSON.parse(secret);
+const admin = parsedSecret.baobab.admin.address;
 
-module.exports = function(deployer, network, accounts) {
-    const owner = accounts[0];
-    const admin = accounts[1];
-
-    var cloudbric = null;
-
-    deployer.deploy(
-        Cloudbric, admin, {from: owner}
-    ).then(function() {
-        return Cloudbric.deployed();    
-    }).then(function(instance) {
-        cloudbric = instance;
-        // interact wiht Cloudbric contract
-        console.log(`Cloudbric deployed at ${cloudbric.address}`);
-    });
+module.exports = function (deployer) {
+    deployer.deploy(Cloudbric, admin)
+        .then(() => {
+            if (Cloudbric._json) {
+                fs.writeFile(
+                    'deployedAbiOfCloudbric',
+                    JSON.stringify(Cloudbric._json, 2),
+                    (err) => {
+                        if (err) throw err
+                        console.log(`The abi of ${Cloudbric._json.contractName} is recorded on deployedAbiOfCloudbric file`)
+                    }
+                )
+            }
+            fs.writeFile(
+                'deployedAddressOfCloudbric',
+                Cloudbric.address,
+                (err) => {
+                    if (err) throw err
+                    console.log(`The deployed contract address * ${Cloudbric.address}`)
+                }
+            )
+        })
 }
